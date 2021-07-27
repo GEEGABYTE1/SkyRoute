5c51b6a0046cdddd860cceefd2115882f6b03834
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-07-27 17:46:35 -0400

    BFS and DFS implementation

---
+++ graph_search.py
@@ -0,0 +1,35 @@
+
+def dfs(graph, current_vertex, target, visited=None):
+    if visited == None:
+        visited = []
+    visited.append(current_vertex)
+    if current_vertex == target:
+        return visited 
+    else:
+        for neighbour in graph[current_vertex]:
+            if not neighbour in visited:
+                path = dfs(graph, neighbour, target, visited)
+
+                if path:
+                    return path 
+
+def bfs(graph, start_vertex, target):
+    path = [start_vertex]
+    vertex_and_path = [start_vertex, target]
+    bfs_queue = [vertex_and_path]
+    visited = set()
+
+    while bfs_queue:
+        current_vertex, path = bfs_queue.pop()
+        visited.add(current_vertex)
+
+        for neighbour in graph[current_vertex]:
+            if not neighbour in visited:
+                if neighbour == target:
+                    path.append(neighbour)
+                    return path 
+                else:
+                    path.append(neighbour)
+                    bfs_queue.append([neighbour, path])
+    
+    return None
